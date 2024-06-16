b3946c630ea4370fafd98baead6dfde5ede2026c
wanglei028 <965581043@qq.com>
2024-06-16 11:08:09 +0800

    vue3 npm create vue@latest

---
+++ vue3/vue3-demo/src/components/Hello.tsx
@@ -0,0 +1,7 @@
+interface HelloProps {
+  name: string;
+}
+
+export default function Hello({ name }: HelloProps) {
+  return <h1>Hello {name}!</h1>;
+}
---
+++ vue3/vue3-demo/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Hello name="Vue 3 + Vite" />
+  </div>
+</template>
+<script setup lang="ts">
+import Hello from '@/components/Hello';
+
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
